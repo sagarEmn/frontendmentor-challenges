3079af6152aad783a68cc72a747e5603499873c7
Sagar <jussagar1@gmail.com>
2024-09-13 14:02:29 +0545

    Add overflow hidden

--- FAQ-Accordion/main.js
+++ FAQ-Accordion/main.js
@@ -5,22 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     wrapper.addEventListener("click", function () {
       const isActive = this.classList.contains("active");
 
-      // process: re-iterates over all the (.container__title-wrapper)
-      // then:
-      // removes 'active' class fro all wrappers
       titleWrappers.forEach((w) => {
         w.classList.remove("active");
-
-        // process: re-iterates over the current div (.container__title-wrapper)
-        // then:
-        // finds the dom with class "container__title-icon"
-        // stores it in const icon
         const icon = w.querySelector(".container__title-icon");
         icon.src = "icon-plus.svg";
       });
 
-    //   adds classlist "active" if doesn't contain active classList
-      if(!isActive) {
+      if (!isActive) {
         this.classList.add("active");
         const icon = this.querySelector(".container__title-icon");
         icon.src = "icon-minus.svg";
