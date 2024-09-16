f5c283b21bdc6d7eabb9b9c8e01c5f28e80387ba
Sagar <jussagar1@gmail.com>
2024-09-16 15:27:44 +0545

    Add event listener, aria-expanded toggle functionality for navbar

--- Manage Landing Page/main.js
+++ Manage Landing Page/main.js
@@ -2,5 +2,9 @@ const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 mobileNavToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? mobileNavToggle.setAttribute("aria-expanded", false)
+    : mobileNavToggle.setAttribute("aria-expanded", true);
+
   primaryNav.toggleAttribute("data-visible");
 });
